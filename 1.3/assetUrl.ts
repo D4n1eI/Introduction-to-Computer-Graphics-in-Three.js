b33df3cbd027cc1d42cb397ae6ff0be785b64f1c
D4n1eI <dan.andrade@ua.pt>
2026-04-29 10:37:44 +0100

    # Update Stable version

--- 1.3/assetUrl.ts
+++ 1.3/assetUrl.ts
@@ -1,7 +1,4 @@
 export function assetUrl(path: string): string {
-  const basePath = import.meta.env.BASE_URL.endsWith("/")
-    ? import.meta.env.BASE_URL
-    : `${import.meta.env.BASE_URL}/`;
-
-  return `${basePath}${path.replace(/^\//, "")}`;
+  const normalizedPath = path.replace(/^\//, "");
+  return new URL(`dist/${normalizedPath}`, document.baseURI).toString();
 }
