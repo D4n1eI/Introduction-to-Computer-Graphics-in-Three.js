8ea23a7af477ffe97c0fa3e415533f9ffdf10d38
D4n1eI <dan.andrade@ua.pt>
2026-05-05 12:13:24 +0100

    Code Cleanup

--- 1.2/HealthBarOptions.ts
+++
@@ -1,7 +0,0 @@
-export interface HealthBarOptions {
-  tilesHoriz?: number;
-  tilesVert?: number;
-  bgFrame?: { row: number; col: number };
-  fillFrame?: { row: number; col: number };
-  offset?: number;
-}
