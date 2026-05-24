74f22d2e7f3e43ba96e56fd9e5837ae39c03d558
D4n1eI <dan.andrade@ua.pt>
2026-05-24 18:55:19 +0100

    Fix GitHub Pages asset URLs

--- src/assetUrl.ts
+++ src/assetUrl.ts
@@ -1,4 +1,4 @@
 export function assetUrl(path: string): string {
   const normalizedPath = path.replace(/^\//, "");
-  return new URL(`dist/${normalizedPath}`, document.baseURI).toString();
+  return new URL(normalizedPath, document.baseURI).toString();
 }
